4ed70d61a8dbed7170d516c13bfd4c78a5053b88
farid3dom <farid.db162@gmail.com>
2025-07-13 17:19:35 +0300

    last ch

--- src/pages/Price/index.jsx
+++ src/pages/Price/index.jsx
@@ -45,30 +45,22 @@ export default function Price() {
         <div className="titles">
           <div className="title_left">
             <h1>Прайслист</h1>
-
             <div className="font-size-controls">
               <button onClick={decreaseFontSize}>A–</button>
               <button onClick={increaseFontSize}>A+</button>
+              <button className="filter-toggle" onClick={() => setFilterOpen(true)}>
+                Категории {filterOpen ? '▲' : '▼'}
+              </button>
             </div>
-
             <div className="search_input">
               <input
                 type="text"
                 placeholder="Поиск..."
                 value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
+                onChange={e => setSearchTerm(e.target.value)}
                 className="search-input"
               />
             </div>
-          </div>
-
-          <div className="categories">
-            <button className="filter-toggle" onClick={() => setFilterOpen(!filterOpen)}>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="48px" height="48px">
-                <path fill="#ecbb47" d="M38,12H22l-4-4H8c-2.2,0-4,1.8-4,4v24c0,2.2,1.8,4,4,4h31c1.7,0,3-1.3,3-3V16C42,13.8,40.2,12,38,12z" />
-                <path fill="#FFCA28" d="M42.2,18H15.3c-1.9,0-3.6,1.4-3.9,3.3L8,40h31.7c1.9,0,3.6-1.4,3.9-3.3l2.5-14C46.6,20.3,44.7,18,42.2,18z" />
-              </svg>
-            </button>
           </div>
         </div>
       </div>
